2fbfcd4809e4e4728cb85d081a7b58b141067f62
Atharva Nagore <atharvanagore2004@gmail.com>
2024-01-15 00:15:16 +0530

    added unexpectederrorhandler and logger for logging the info and errors

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,36 @@
 import app from "./app.js";
+import logger from "./configs/logger.config.js";
 
 const PORT = process.env.PORT || 8000;
 
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 
-app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+let server;
+
+server = app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
+});
+
+const exitHandler = () => {
+  if (server) {
+    logger.info("Server closed.");
+    process.exit(1);
+  }else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error) => {
+  logger.error(error);
+  exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
+process.on("SIGTERM", () => {
+    if (server) {
+      logger.info("Server closed.");
+      process.exit(1);
+    }
+  });
